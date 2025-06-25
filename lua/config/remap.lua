a53185e55730c872b4f583fd0be671cc1379712a
I-Missha <i.missha.d@gmail.com>
2025-06-25 16:36:37 +0700

    harpoon config

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- Temporarily map <C-,> to something simple (e.g., echo)
+vim.keymap.set("n", "<C-,>", "<cmd>echo 'Ctrl+, works!'<CR>")
